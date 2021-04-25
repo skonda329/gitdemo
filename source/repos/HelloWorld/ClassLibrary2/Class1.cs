91e5ef6600760c2846652ef8fc78548b65a781da
konda <skonda329@gmail.com>
2021-04-24 22:11:34 -0400

    first

---
+++ source/repos/HelloWorld/ClassLibrary2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+class program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+
+
---
+++ source/repos/HelloWorld/HelloWorld/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("helloworld");
+    }
+}
